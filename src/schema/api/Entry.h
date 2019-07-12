662be55f4487af642a1d69f699afa3ec82b587ac
Toby <toby.james.thomas@gmail.com>
2019-07-11 23:33:27 -0400

    Define Entry class, create EntryType.h

--- src/schema/api/Entry.h
+++ src/schema/api/Entry.h
@@ -1,10 +1,41 @@
 #ifndef LOODB_ENTRY_H
 #define LOODB_ENTRY_H
 
+#include "EntryType.h"
+#include <memory>
+
+// Private members of Entry
+struct EntryImpl {
+  EntryType type_;
+};
 
 class Entry {
 
-  // TODO
+    // pImpl for Entry
+    std::unique_ptr<EntryImpl> impl_;
+
+  public:
+
+    // Constructor
+    Entry( /* params */ );
+
+    // Overload less than operator
+    bool operator<(const Entry&);
+
+    // Overload less than or equals operator
+    bool operator<=(const Entry&);
+
+    // Overload greater than operator
+    bool operator>(const Entry&);
+
+    // Overload greater than or equals operator
+    bool operator>=(const Entry&);
+
+    // Overload equals operator
+    bool operator==(const Entry&);
+
+    // Overload not equals operator
+    bool operator!=(const Entry&);
 
 };
 
---
+++ src/schema/api/EntryType.h
@@ -0,0 +1,10 @@
+#ifndef LOODB_ENTRYTYPE_H
+#define LOODB_ENTRYTYPE_H
+
+enum EntryType {
+  NULL,
+  INTEGER,
+  TEXT
+};
+
+#endif // LOODB_ENTRYTYPE_H
