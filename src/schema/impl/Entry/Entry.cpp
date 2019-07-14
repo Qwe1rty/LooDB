57c49b11ee451c2b9a66862944d04ccaadb3344a
Toby <toby.james.thomas@gmail.com>
2019-07-14 17:38:04 -0400

    Add implementations for Entry and children

---
+++ src/schema/impl/Entry/Entry.cpp
@@ -0,0 +1,32 @@
+#include "../../api/Entry/Entry.h"
+#include "../../api/Entry/EntryType.h"
+
+Entry::Entry(EntryType type) : type_(type) {}
+
+EntryType Entry::getType() const {
+    return type_;
+}
+
+bool Entry::operator<(const Entry& other) const {
+  return this->less(other);
+}
+
+bool Entry::operator<=(const Entry& other) const {
+  return this->lesseq(other);
+}
+
+bool Entry::operator>(const Entry& other) const {
+  return this->greater(other);
+}
+
+bool Entry::operator>=(const Entry& other) const {
+  return this->greatereq(other);
+}
+
+bool Entry::operator==(const Entry& other) const {
+  return this->equal(other);
+}
+
+bool Entry::operator!=(const Entry& other) const {
+  return this->notequal(other);
+}
