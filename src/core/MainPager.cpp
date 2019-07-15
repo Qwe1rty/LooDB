fe91d834e3ced6070242fbb0eba4370c0db1275e
cchoi <caleb.choi@uwaterloo.ca>
2019-07-15 01:58:26 -0400

    Small comments

--- src/core/MainPager.cpp
+++ src/core/MainPager.cpp
@@ -8,7 +8,7 @@ int main() {
 
   Pager pager{"toast.txt"};
 
-  std::unique_ptr<Page> page = std::make_unique<BPTreeHeaderPage>(123);
+  std::unique_ptr<Page> page = std::make_unique<BPTreeHeaderPage>(124);
 
   pager.write(0, page);
   const auto read = pager.read(0);
@@ -16,7 +16,7 @@ int main() {
   const auto* cast = dynamic_cast<BPTreeHeaderPage*>(read.get());
 
   std::cout << cast->type() << std::endl; // 0
-  std::cout << cast->root_ << std::endl; // 123
+  std::cout << cast->root_ << std::endl; // 124, or seen as a pipe character in the file
 
   return 0;
 }
--- src/filesystem/pagination/interface/impl/Pager.cpp
+++ src/filesystem/pagination/interface/impl/Pager.cpp
@@ -26,6 +26,7 @@ private:
   std::fstream stream_;
 };
 
+// https://stackoverflow.com/questions/9954518/stdunique-ptr-with-an-incomplete-type-wont-compile
 void Pager::ImplDeleter::operator()(Pager::Impl* impl) {
   delete impl;
 }
