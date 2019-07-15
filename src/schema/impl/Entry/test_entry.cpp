eb5d2df178e65d87efc7b9ff0f087310020b5bf1
Toby <toby.james.thomas@gmail.com>
2019-07-14 23:18:20 -0400

    Add test cases for EntryCodec

--- src/schema/impl/Entry/test_entry.cpp
+++ src/schema/impl/Entry/test_entry.cpp
@@ -2,6 +2,7 @@
 #include "../../api/Entry/IntEntry.h"
 #include "../../api/Entry/StringEntry.h"
 #include "../../api/Entry/NullEntry.h"
+#include "../../api/Entry/EntryCodec.h"
 #include <iostream>
 #include <string>
 #include <memory>
@@ -10,7 +11,7 @@
 
 using namespace std;
 
-int main() {
+void test_entry_equality() {
   unique_ptr<Entry> int1 = make_unique<IntEntry>(5);
   unique_ptr<Entry> int2 = make_unique<IntEntry>(5);
   unique_ptr<Entry> text1 = make_unique<StringEntry>("Hello");
@@ -30,7 +31,16 @@ int main() {
   cout << "null2 type: " << null2->getType() << endl;
   cout << "null1 == null2: " << (*null1 == *null2) << endl;
 
-  cout << "int1 == null1: " << (*int1 == *null1) << endl;
+  cout << "int1 == null1: " << (*int1 == *null1) << endl;  
+}
+
+void test_entry_vectors() {
+  unique_ptr<Entry> int1 = make_unique<IntEntry>(5);
+  unique_ptr<Entry> int2 = make_unique<IntEntry>(5);
+  unique_ptr<Entry> text1 = make_unique<StringEntry>("Hello");
+  unique_ptr<Entry> text2 = make_unique<StringEntry>("World");
+  unique_ptr<Entry> null1 = make_unique<NullEntry>();
+  unique_ptr<Entry> null2 = make_unique<NullEntry>();
 
   vector<unique_ptr<Entry>> entries;
 
@@ -46,10 +56,47 @@ int main() {
   }
 
   vector<vector<unique_ptr<Entry>>> entries2;
-
   entries2.emplace_back(move(entries));
-
   vector<vector<unique_ptr<Entry>>> entries3;
-
   entries3 = move(entries2);
+}
+
+void test_entrycodec() {
+  EntryCodec CODEC = EntryCodec();
+
+  unique_ptr<Entry> null3 = make_unique<NullEntry>();
+  vector<char> encoded_null3 = CODEC.encode(null3);
+  for (int i = 0; i < encoded_null3.size(); ++i) {
+    cout << encoded_null3[i];
+  }
+  cout << endl;
+  unique_ptr<Entry> decoded_null3 = CODEC.decode(encoded_null3);
+  cout << "null3 == decoded_null3: " << (*null3 == *decoded_null3) << endl;
+
+  unique_ptr<Entry> int3 = make_unique<IntEntry>(-1234);
+  vector<char> encoded_int3 = CODEC.encode(int3);
+  for (int i = 0; i < encoded_int3.size(); ++i) {
+    cout << encoded_int3[i];
+  }
+  cout << endl;
+  unique_ptr<Entry> decoded_int3 = CODEC.decode(encoded_int3);
+  cout << "int3 == decoded_int3: " << (*int3 == *decoded_int3) << endl;
+
+  unique_ptr<Entry> text3 = make_unique<StringEntry>("Aloha");
+  vector<char> encoded_text3 = CODEC.encode(text3);
+  for (int i = 0; i < encoded_text3.size(); ++i) {
+    cout << encoded_text3[i];
+  }
+  cout << endl;
+  unique_ptr<Entry> decoded_text3 = CODEC.decode(encoded_text3);
+  cout << "text3 == decoded_text3: " << (*text3 == *decoded_text3) << endl;
+}
+
+int main() {
+  
+  test_entry_equality();
+
+  test_entry_vectors();
+
+  test_entrycodec();
 }
