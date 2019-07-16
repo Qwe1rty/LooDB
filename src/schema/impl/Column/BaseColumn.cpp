52b310843b4fab45431102893accdb8cf663311c
cchoi <caleb.choi@uwaterloo.ca>
2019-07-16 00:16:23 -0400

    Set up a fetch suite of functions + other fixes and improvements to the Column implementation

--- src/schema/impl/Column/BaseColumn.cpp
+++ src/schema/impl/Column/BaseColumn.cpp
@@ -5,71 +5,118 @@
 #include "../../../schema/api/Entry/EntryCodec.h"
 
 
-struct BaseColumn::Impl {
+class BaseColumn::Impl {
+
+public:
+
+  bool valid_(const Entry&) const;
+  uint32_t read_(uint32_t);
+  void write_(uint32_t entry_index, uint32_t row_index);
+  bool empty_();
+
+private:
 
   std::string name_;
-  const EntryType type_;
+  const EntryType entry_type_;
+  const EntryCodec entry_codec_;
   Pager index_file_;
   Pager& data_file_;
+
+  std::unique_ptr<BTreeHeaderPage> fetch_header();
+  std::unique_ptr<BTreeNodePage> fetch_node(int32_t);
+  std::unique_ptr<Entry> fetch_entry(int32_t);
+  
+  void split(uint32_t, BTreeNodePage& root, BTreeNodePage& child);
+  void insert_capacity(uint32_t row_index, 
+                       uint32_t entry_index, 
+                       uint32_t node_index);
 };
 
 
 /*
- * BTree helper functions
+ * Private member functions
  */
 
-namespace {
+std::unique_ptr<BTreeHeaderPage> BaseColumn::Impl::fetch_header() {
+  return index_file_.fetch<BTreeHeaderPage>(0);
+}
 
-  // Split a child node into 2
-  // TODO possible just pass in the root
-  void split(Pager& pager,
-             uint32_t split_index,
-             BTreeNodePage& root,
-             BTreeNodePage& child) {
+std::unique_ptr<BTreeNodePage> BaseColumn::Impl::fetch_node(int32_t index) {
+  return index_file_.fetch<BTreeNodePage>(index);
+}
 
-    // Copy largest (BTreeNodePage::ORDER - 1) elements of the child into a right counterpart
-    auto right_split = std::make_unique<BTreeNodePage>(
-      child.leaf_,
-      child.right_,
-      std::vector<Cell>(
-        child.node_.cbegin() + BTreeNodePage::ORDER - 1, // The middle element
-        child.node_.cend()
-      )
-    );
+std::unique_ptr<Entry> BaseColumn::Impl::fetch_entry(int32_t index) {
+  return entry_codec_.decode(data_file_.fetch<EntryPage>(index)->value_);
+}
 
-    // Erase the elements from the child that are now in right_split, and update the child's
-    // right pointer
-    child.node_.erase(
-      child.node_.cbegin() + BTreeNodePage::ORDER,
+// Split a child node into 2, TODO possibly just pass in the root
+void BaseColumn::Impl::split(uint32_t split_index, BTreeNodePage& root, BTreeNodePage& child) {
+
+  // Copy largest (BTreeNodePage::ORDER - 1) elements of the child into a right counterpart
+  auto right_split = std::make_unique<BTreeNodePage>(
+    child.leaf_,
+    child.right_,
+    std::vector<Cell>(
+      child.node_.cbegin() + BTreeNodePage::ORDER - 1, // The middle element
       child.node_.cend()
-    );
-    child.right_ = child.node_.at(BTreeNodePage::ORDER - 1).left_;
+    )
+  );
 
-    // Insert the middle element of the child into the i'th slot of the parent,
-    // updating the page numbers
-    root.node_.insert(
-      root.node_.cbegin() + split_index,
-      child.node_.at(BTreeNodePage::ORDER - 1)
-    );
-    root.node_.at(split_index).left_ = root.node_.at(split_index + 1).left_;
+  // Erase the elements from the child that are now in right_split, and update the child's
+  // right pointer
+  child.node_.erase(
+    child.node_.cbegin() + BTreeNodePage::ORDER,
+    child.node_.cend()
+  );
+  child.right_ = child.node_.at(BTreeNodePage::ORDER - 1).left_;
 
-    // Write the new child's page, and update the i+1'th cell in the root to point to the
-    // new appended node
-    root.node_.at(split_index + 1).left_ = pager.size();
-    pager.append(std::move(right_split));
+  // Insert the middle element of the child into the i'th slot of the parent,
+  // updating the page numbers
+  root.node_.insert(
+    root.node_.cbegin() + split_index,
+    child.node_.at(BTreeNodePage::ORDER - 1)
+  );
+  root.node_.at(split_index).left_ = root.node_.at(split_index + 1).left_;
 
-    // Erase the middle element of the child now that it's stored in the parent
-    child.node_.erase(child.node_.cbegin() + (BTreeNodePage::ORDER - 1));
+  // Write the new child's page, and update the i+1'th cell in the root to point to the
+  // new appended node
+  root.node_.at(split_index + 1).left_ = index_file_.size();
+  index_file_.append(std::move(right_split));
+
+  // Erase the middle element of the child now that it's stored in the parent
+  child.node_.erase(child.node_.cbegin() + (BTreeNodePage::ORDER - 1));
+}
+
+// Inserts a key without a node that is not full (so it doesn't immediately need splitting)
+void BaseColumn::Impl::insert_capacity(const uint32_t row_index,
+                                       const uint32_t entry_index,
+                                       const uint32_t node_index) {
+
+  // Fetch pages
+  const auto node = fetch_node(node_index);
+  const auto entry = fetch_entry(entry_index);
+
+  // Search index
+  uint32_t i = node->node_.size() - 1;
+
+  if (node->leaf_) {
+
+    for (; i >= 0 && fetch_entry(node->node_.at(i).key_) > entry; --i) {
+      node->node_.at(i + 1) = node->node_.at(i);
+    }
+    node->node_.at(i + 1) = Cell{
+      entry_index,
+      row_index,
+      0
+    };
   }
 
-  // Inserts a key without a node that is not full (so it doesn't immediately need splitting)
-  void insert_capacity(Pager& pager,
-                       uint32_t insert_index,
-                       BTreeNodePage& node,
-                       const std::unique_ptr<Entry>& entry) {
+  else {
 
+    while (i >= 0 && fetch_entry(node->node_.at(i).key_) > entry) --i;
 
   }
+
 }
 
 
@@ -77,17 +124,17 @@ namespace {
  * Public member functions
  */
 
-bool BaseColumn::valid_(const Entry& entry) const {
+bool BaseColumn::Impl::valid_(const Entry& entry) const {
 
-  return entry.getType() == impl_->type_;
+  return entry.getType() == entry_type_;
 }
 
-uint32_t BaseColumn::read_(uint32_t) {
+uint32_t BaseColumn::Impl::read_(uint32_t) {
 
   return 102931258;
 }
 
-void BaseColumn::write_(uint32_t entry_index, uint32_t row_index) {
+void BaseColumn::Impl::write_(uint32_t entry_index, uint32_t row_index) {
 
   if (empty_()) {
 
@@ -97,19 +144,19 @@ void BaseColumn::write_(uint32_t entry_index, uint32_t row_index) {
       0,
       std::vector<Cell>{}
     );
-    impl_->index_file_.append(std::move(root_node));
+    index_file_.append(std::move(root_node));
 
     // Update the header's empty_ and root_ fields
-    auto header = impl_->index_file_.fetch<BTreeHeaderPage>(0);
+    auto header = fetch_header();
     header->empty_ = false;
-    header->root_ = impl_->index_file_.size() - 1;
-    impl_->index_file_.write(0, std::move(header));
+    header->root_ = index_file_.size() - 1;
+    index_file_.write(0, std::move(header));
   }
 
   else {
 
-    auto header = impl_->index_file_.fetch<BTreeHeaderPage>(0);
-    auto old_root = impl_->index_file_.fetch<BTreeNodePage>(header->root_);
+    auto header = fetch_header();
+    auto old_root = fetch_node(header->root_);
 
     // If the node is full
     if (old_root->node_.size() >= 2 * BTreeNodePage::ORDER + 1) {
@@ -127,45 +174,58 @@ void BaseColumn::write_(uint32_t entry_index, uint32_t row_index) {
         }
       );
 
-      split(impl_->index_file_, 0, *new_root, *old_root);
+      split(0, *new_root, *old_root);
 
       // Compare the key to be inserted and the previous root's middle key
       uint64_t middle_index{new_root->node_.at(0).key_};
-      const EntryCodec codec{};
-      const auto middle_entry = codec.decode(impl_->index_file_.fetch<EntryPage>(middle_index)->value_);
-      const auto insert_entry = codec.decode(impl_->index_file_.fetch<EntryPage>(entry_index)->value_);
+      const auto middle_entry = fetch_entry(middle_index);
+      const auto insert_entry = fetch_entry(entry_index);
 
       uint32_t i = middle_entry < insert_entry;
-      insert_capacity(impl_->index_file_, i, insert_entry);
+      insert_capacity(row_index, entry_index, new_root->node_.at(i).left_);
 
+      // TODO Write back all modified pages!!!!!!!!!
       // Append the new root, and update the header to point to it
 
     }
 
-    else {
-
-    }
+    else insert_capacity(row_index, entry_index, header->root_);
   }
 }
 
-bool BaseColumn::empty_() const {
+bool BaseColumn::Impl::empty_() {
 
   // Contains more than just the header page
-  return impl_->index_file_.fetch<BTreeHeaderPage>(0)->empty_;
+  return fetch_header()->empty_;
 }
 
 
 /*
- * Constructor
+ * BaseColumn.h implementations
  */
+
+bool BaseColumn::valid_(const Entry& entry) const {
+  return impl_->valid_(entry);
+}
+
+uint32_t BaseColumn::read_(uint32_t index) {
+  return impl_->read_(index);
+}
+
+void BaseColumn::write_(uint32_t entry_index, uint32_t row_index) {
+  impl_->write_(entry_index, row_index);
+}
+
+bool BaseColumn::empty_() const {
+  return impl_->empty_();
+}
 
 BaseColumn::BaseColumn(const std::string& name, EntryType type, const std::unique_ptr<Pager>& data_file) :
   impl_{std::make_unique<Impl>(
       name,
       type,
+      EntryCodec{},
       Pager{name, Page::SIZE},
       data_file
   )}
-{
-
-}
+{}
