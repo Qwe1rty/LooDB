70ac29822886f241243b3ae22dde2095e45a4583
Toby <toby.james.thomas@gmail.com>
2019-07-18 05:13:29 -0400

    Update Table.h to include map of col names to entry type, map of col names to order index, printColumns, where() and Cursor begin() and end()

--- src/schema/api/Table.h
+++ src/schema/api/Table.h
@@ -4,31 +4,59 @@
 //#include "Column/Column.h"
 //#include "Column/BaseColumn.h"
 #include "Cursor.h"
+#include "Column/Column.h"
 #include "Entry/Entry.h"
 #include "Entry/EntryType.h"
+#include "../../interpreter/statements/api/Select.h"
 #include <memory>
 #include <string>
-#include <map>
+#include <unordered_map>
+#include <set>
 
 class Table {
   // Private members of Table
   struct TableImpl {
-    // paths and file extensions
-    std::string path_ = "./loo/";
-    const std::string data_ext_ = ".data";
-    const std::string col_ext_ = ".col";
-    const std::string row_ext_ = ".row";
-    // our private variables
-    std::string name_;
-    std::map<std::string, int> columns_; // replace int with column class
-    std::string pkey_column_;
-    std::string data_file_ = ".";
-    std::string row_file_ = ".";
-    TableImpl(std::string);
-    //populate table with existing ones in the directory
-    void populateTable();
-    // build table from command (create table ...)
-    void buildTable();
+      // Paths and file extensions
+      std::string path_ = "./loo/";
+      const std::string data_ext_ = ".data";
+      const std::string col_ext_ = ".col";
+      const std::string row_ext_ = ".row";
+
+      // Private variables
+      std::string name_;
+      std::unordered_map<std::string, std::unique_ptr<Column>> columns_;
+      std::unordered_map<std::string, EntryType> columnsTypes_;
+      std::unordered_map<std::string, int> columnsIndices_;
+      std::string pkey_column_;
+      std::string data_file_ = ".";
+      std::string row_file_ = ".";
+
+      // Constructor
+      TableImpl(std::string);
+
+      // Populate table with existing ones in the directory
+      void populateTable();
+      // Build table from command (create table ...)
+      void buildTable();
+      // Print tables given select criteria
+      void printColumns_(std::vector<string>& columns, bool where,
+                         unique_ptr<SQLSelect::WhereTree>& whereTree);
+
+      // Select helper for where clause
+      // Given a where tree, returns a ordered set of B+ page indices
+      // Page indices are in order so that rows print in same order as tablename.data
+      std::set<uint32_t> where(std::unique_ptr<SQLSelect::WhereTree>& root);
+
+      // Begin and end for Cursor
+      Cursor begin();
+      Cursor end();
+
+    private:
+
+      // Helper functions for where()
+      std::set<uint32_t> whereHelper(std::unique_ptr<SQLSelect::WhereTree>& root);
+      std::set<uint32_t> setUnion(std::set<uint32_t>& set1, std::set<uint32_t>& set2);
+      std::set<uint32_t> setIntersection(std::set<uint32_t>& set1, std::set<uint32_t>& set2);
   };
 
   // pImpl for Table
@@ -38,6 +66,12 @@ class Table {
 
   // Constructor
   Table(std::string);
+
+  // Print rows given select statement criteria
+  // Prints nothing if table is empty
+  // Prints error if where clauses don't match column type
+  void printColumns(std::vector<string>& columns, bool where,
+                    unique_ptr<SQLSelect::WhereTree>& whereTree);
 
   // Return a Cursor to row at Entry in table column
   //Cursor find(std::string, Entry);
--- src/schema/impl/Table.cpp
+++ src/schema/impl/Table.cpp
@@ -1,9 +1,18 @@
 #include "../api/Table.h"
+#include "../api/Cursor.h"
+#include "../api/Column/Column.h"
+#include "../api/Entry/Entry.h"
+#include "../api/Entry/IntEntry.h"
 #include <sys/stat.h>
 #include <dirent.h>
 #include <iostream>
 #include <string.h>
 #include <exception>
+#include <algorithm>
+#include <iterator>
+#include <set>
+#include <tuple>
+
 //#include "../../filesystem/pagination/interface/api/Pager.h"
 using namespace std;
 
@@ -52,4 +61,166 @@ void Table::TableImpl::populateTable(){
 
 void Table::TableImpl::buildTable(){
   // to be used by create table command
+}
+
+std::set<uint32_t> Table::TableImpl::setUnion(std::set<uint32_t>& set1, std::set<uint32_t>& set2) {
+  // Declare union set
+  std::set<uint32_t> union_;
+
+  // Get union of set1 and set2
+  std::set_union(set1.begin(), set1.end(),
+                 set2.begin(), set2.end(),
+                 std::inserter(union_, union_.begin()));
+
+  return union_;
+}
+
+std::set<uint32_t> Table::TableImpl::setIntersection(std::set<uint32_t>& set1, std::set<uint32_t>& set2) {
+  // Declare intersection set
+  std::set<uint32_t> intersection_;
+
+  // Get union of set1 and set2
+  std::set_union(set1.begin(), set1.end(),
+                 set2.begin(), set2.end(),
+                 std::inserter(intersection_, intersection_.begin()));
+
+  return intersection_;
+}
+
+// Select helper for where clause
+// Given a where tree, returns a ordered set of B+ page indices
+// Page indices are in order so that rows print in same order as tablename.data
+std::set<uint32_t> Table::TableImpl::whereHelper(std::unique_ptr<SQLSelect::WhereTree>& root) {
+  // Declare indices set
+  std::set<uint32_t> indices;
+
+  // Is a where expression
+  if (root->leaf_) {
+    // Get column name and entry
+    const std::string& columnName = std::get<0>(root->expr_);
+    const std::unique_ptr<Entry>& entry = std::get<1>(root->expr_);
+
+    // Throw if entry doesn't match column type
+    if (entry->getType() != this->columnsTypes_[columnName]) {
+      // Invariance: column is of type INTEGER or TEXT
+      EntryType columnType = this->columnsTypes_[columnName];
+      std::string type = columnType == EntryType::INTEGER ? "integer" : "text";
+      throw std::invalid_argument("Error: Column " + columnName + " is of type " + type);
+    }
+
+    const std::unique_ptr<Column>& column = this->columns_[columnName];
+
+    // Find entry in column
+    auto iter = column->find(*entry);
+
+    // While iter meets where condition, insert page index into indices
+    while (*iter != *column->end() && *((**iter).first) == *entry) {
+      std::cout << static_cast<IntEntry&>(*((**iter).first)).getVal();
+      std::cout << " == ";
+      std::cout << static_cast<IntEntry&>(*entry).getVal();
+      std::cout << std::endl;
+      indices.insert((**iter).second);
+      ++(*iter);
+    }
+
+    return indices;
+  }
+
+  // Is a where operation
+
+  // Get LHS where indices
+  std::set<uint32_t> l_indices = whereHelper(root->where1_);
+  // Get RHS where indices
+  std::set<uint32_t> r_indices = whereHelper(root->where2_);
+
+  // Get union or intersection of wheres
+  if (root->op_ == "AND") {
+    indices = setIntersection(l_indices, r_indices);
+  } else { // "OR"
+    indices = setUnion(l_indices, r_indices);
+  }
+
+  return indices;
+}
+
+// Given a where tree, returns a ordered set of B+ page indices
+// Page indices are in order so that rows print in same order as tablename.data
+std::set<uint32_t> Table::TableImpl::where(std::unique_ptr<SQLSelect::WhereTree>& root) {
+  // Get page indices
+  std::set<uint32_t> indices = whereHelper(root);
+  // Return primary keys
+  return indices;
+}
+
+// Print rows given select statement criteria
+void Table::TableImpl::printColumns_(std::vector<string>& columns, bool where,
+                                     unique_ptr<SQLSelect::WhereTree>& whereTree) {
+  // Check that columns exist
+  for (std::string& column : columns) {
+    if (this->columns_.find(column) == this->columns_.end()) {
+      // Column doesn't exist, so print error and return
+      std::cout << "Error: Column \"" << column << "\"";
+      std::cout << " doesn't exist in " << this->name_ << std::endl;
+      return;
+    }
+  }
+
+  // Get page indices from whereTree if where == true
+  std::set<uint32_t> pageIndices;
+  if (where) {
+    try {
+      // this->where() will throw if try to equate
+      // a column to the wrong type
+      pageIndices = this->where(whereTree);
+    } catch (std::invalid_argument e) {
+      // Catch invalid argument error, print and return
+      std::cout << e.what() << std::endl;
+      return;
+    }
+  }
+
+  // Print columns using cursor
+  // If where == true, print individual rows
+  // Otherwise, print every row using cursor
+  if (where) {
+    std::cout << "where" << std::endl;
+  } else {
+    for (auto it = this->begin(); it != this->end(); ++it) {
+      std::vector<std::unique_ptr<Entry>> row = std::move(*it);
+
+      // Print given columns
+      // EntryType entryType;
+      // for (int i = 0; i < .size(); ++i) {
+      //   entryType = row[i]->getType();
+      //   if (entryType == EntryType::NULL_) {
+      //     std::cout << "null" << std::endl;
+      //   } else if (entryType == EntryType::INTEGER) {
+      //     std::cout << static_cast<IntEntry&>(*row[i]).getVal() << std::endl;
+      //   } else if (entryType == EntryType::TEXT) {
+      //     std::cout << static_cast<IntEntry&>(*row[i]).getVal() << std::endl;
+      //   }
+      //   std::cout << 
+      // }
+
+      std::cout << "no where" << std::endl;
+    }
+  }
+}
+
+// Print rows given select statement criteria
+void Table::printColumns(std::vector<string>& columns, bool where,
+                         unique_ptr<SQLSelect::WhereTree>& whereTree) {
+  this->impl_->printColumns_(columns, where, whereTree);
+}
+
+Cursor Table::TableImpl::begin() {
+  std::string rowPath = this->data_file_ + this->data_ext_;
+  std::string dataPath = this->row_file_ + this->row_ext_;
+  return Cursor(rowPath, dataPath, false);
+}
+
+Cursor Table::TableImpl::end() {
+  std::string rowPath = this->data_file_ + this->data_ext_;
+  std::string dataPath = this->row_file_ + this->row_ext_;
+  return Cursor(rowPath, dataPath, true);
 }
