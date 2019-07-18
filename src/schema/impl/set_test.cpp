27790d27cafbfe955036389d58f0bba52728023f
Toby <toby.james.thomas@gmail.com>
2019-07-18 07:26:25 -0400

    comment out set_test main()

--- src/schema/impl/set_test.cpp
+++ src/schema/impl/set_test.cpp
@@ -111,30 +111,30 @@ std::set<uint32_t> where(std::vector<std::pair<std::unique_ptr<Entry>, uint32_t>
   return pks;
 }
 
-int main() {
-  union_intersection();
+// int main() {
+//   union_intersection();
 
-  std::vector<std::pair<std::unique_ptr<Entry>, uint32_t>> col1;
-  col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 5));
-  col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 6));
-  col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 8));
-  col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(6), 9));
-  col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(6), 10));
+//   std::vector<std::pair<std::unique_ptr<Entry>, uint32_t>> col1;
+//   col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 5));
+//   col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 6));
+//   col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(5), 8));
+//   col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(6), 9));
+//   col1.emplace_back(std::make_pair(std::make_unique<IntEntry>(6), 10));
 
-  std::vector<std::pair<std::unique_ptr<Entry>, uint32_t>> col2;
-  col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(7), 5));
-  col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(7), 8));
-  col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(8), 9));
-  col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(9), 10));
-  col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(10), 10));
+//   std::vector<std::pair<std::unique_ptr<Entry>, uint32_t>> col2;
+//   col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(7), 5));
+//   col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(7), 8));
+//   col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(8), 9));
+//   col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(9), 10));
+//   col2.emplace_back(std::make_pair(std::make_unique<IntEntry>(10), 10));
 
-  auto where1 = std::make_unique<SQLSelect::WhereTree>("col1", std::make_unique<IntEntry>(5));
-  auto where2 = std::make_unique<SQLSelect::WhereTree>("col2", std::make_unique<IntEntry>(7));
-  auto where3 = std::make_unique<SQLSelect::WhereTree>("col1", std::make_unique<IntEntry>(6));
-  auto where4 = std::make_unique<SQLSelect::WhereTree>("AND", std::move(where1), std::move(where2));
-  auto where5 = std::make_unique<SQLSelect::WhereTree>("OR", std::move(where4), std::move(where3));
+//   auto where1 = std::make_unique<SQLSelect::WhereTree>("col1", std::make_unique<IntEntry>(5));
+//   auto where2 = std::make_unique<SQLSelect::WhereTree>("col2", std::make_unique<IntEntry>(7));
+//   auto where3 = std::make_unique<SQLSelect::WhereTree>("col1", std::make_unique<IntEntry>(6));
+//   auto where4 = std::make_unique<SQLSelect::WhereTree>("AND", std::move(where1), std::move(where2));
+//   auto where5 = std::make_unique<SQLSelect::WhereTree>("OR", std::move(where4), std::move(where3));
 
-  std::set<uint32_t> pks = where(col1, col2, where5);
+//   std::set<uint32_t> pks = where(col1, col2, where5);
 
-  printSet(pks);
-}
+//   printSet(pks);
+// }
